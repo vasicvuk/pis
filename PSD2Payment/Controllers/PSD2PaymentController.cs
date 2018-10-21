13df6ea95bba1e8cf87f146be357378bf75afa04
Vuk Vasić <vuk.vasic@asseco-see.rs>
2018-10-21 17:33:47 +0200

    Add logging support

--- PSD2Payment/Controllers/PSD2PaymentController.cs
+++ PSD2Payment/Controllers/PSD2PaymentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PSD2Payment.Application.Command;
 using PSD2Payment.Filters;
 using PSD2Payment.Repository;
@@ -16,9 +17,14 @@ namespace PSD2Payment.Controllers
     {
         private readonly IAccountRepository _accountRepository;
         private readonly IPaymentRepository _paymentRepository;
+        private readonly ILogger<PSD2PaymentController> _logger;
 
-        public PSD2PaymentController(IAccountRepository accountRepository, IPaymentRepository paymentRepository)
+        public PSD2PaymentController(
+            IAccountRepository accountRepository,
+            IPaymentRepository paymentRepository,
+            ILogger<PSD2PaymentController> logger)
         {
+            _logger = logger;
             _accountRepository = accountRepository;
             _paymentRepository = paymentRepository;
         }
@@ -26,6 +32,7 @@ namespace PSD2Payment.Controllers
         [HttpGet("accounts")]
         public IActionResult GetAccounts()
         {
+            _logger.LogInformation("Getting information about accounts");
             return Ok(_accountRepository.GetAccountList());
         }
 
@@ -33,8 +40,10 @@ namespace PSD2Payment.Controllers
         public IActionResult GetAccountInformation([FromBody] RequestAccountInformationCommand ibanCommand)
         {
             var result = _accountRepository.GetAccount(ibanCommand.Iban);
+            _logger.LogInformation("Getting account information for iban: {iban}", ibanCommand.Iban);
             if (result == null)
             {
+                _logger.LogError("Could not find account with provided iban: {iban}", ibanCommand.Iban);
                 return BadRequest(new { error = "Could not find account with provided IBAN" });
             }
             return Ok(result);
@@ -48,15 +57,20 @@ namespace PSD2Payment.Controllers
             switch (paymentResult)
             {
                 case PaymentCommandResult.ACCOUNT_NOT_FOUND:
+                    _logger.LogError("Provided account: {account} not found", command.DebtorAccount.Iban);
                     return BadRequest(new { error = "Account specified not found" });
                 case PaymentCommandResult.INSUFISHENT_FUNDS:
+                    _logger.LogError("There is no enough funds on account {account}", command.DebtorAccount.Iban);
                     return BadRequest(new { error = "No funds for translation on specified account" });
                 case PaymentCommandResult.NO_CURRENCY_ACCOUNT:
+                    _logger.LogError("No specified currency on account: {account}", command.DebtorAccount.Iban);
                     return BadRequest(new { error = "Account for currency not found" });
                 case PaymentCommandResult.OK:
+                    _logger.LogInformation("Translation completed from account: {account} to account: {creditorAccount}", command.DebtorAccount.Iban, command.CreditorAccount.Iban);
                     var result = _accountRepository.GetAccount(command.DebtorAccount.Iban);
                     return Ok(result);
                 default:
+                    _logger.LogCritical("Critical error with transaction from account: {account}", command.DebtorAccount.Iban);
                     return BadRequest(new { error = "Temporary error" });
             }
         }
--- PSD2Payment/Program.cs
+++ PSD2Payment/Program.cs
@@ -1,24 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Graylog;
+using Serilog.Sinks.Graylog.Core;
+using Serilog.Sinks.Graylog.Core.Transport;
 
 namespace PSD2Payment
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var centralLogHost = Environment.GetEnvironmentVariable("CENTRAL_LOG_HOST"); 
+            var loggerConfig = new LoggerConfiguration()
+             .MinimumLevel.Debug()
+             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+             .Enrich.FromLogContext()
+            .WriteTo.Console(); ;
+            if (centralLogHost != null)
+            {
+                var centralLogPort = Environment.GetEnvironmentVariable("CENTRAL_LOG_PORT");
+                var centralLogProtocol = Environment.GetEnvironmentVariable("CENTRAL_LOG_PROTOCOL");
+                TransportType transportType = TransportType.Http;
+                if (centralLogProtocol != null && centralLogProtocol.Equals("Udp"))
+                {
+                    transportType = TransportType.Udp;
+                }
+                loggerConfig.WriteTo.Graylog(new GraylogSinkOptions
+                {
+                    Port = int.Parse(centralLogPort),
+                    HostnameOrAddress = centralLogHost,
+                    TransportType = transportType
+                });
+            }
+            Log.Logger = loggerConfig.CreateLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
